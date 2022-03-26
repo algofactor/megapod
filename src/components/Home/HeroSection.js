62e98c9653bdae626f8760f38626993e804a719f
algofactor <johnsolutions4@gmail.com>
2022-03-26 17:07:58 +0100

    Added responsiveness to the hero section

--- src/components/Home/HeroSection.js
+++ src/components/Home/HeroSection.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { IoCalendarNumberOutline } from "react-icons/io5";
 import { FaShareAlt, FaDownload } from "react-icons/fa";
 import Music1 from '../../music-files/1.mp3'
+import { screens } from "../../data/Screens";
 
 const HeroContainer = styled.div`
 	background-image: url(${HeroBg});
@@ -20,16 +21,26 @@ const HeroContainer = styled.div`
 const HeroContent = styled.div`
 	padding: 8rem 2rem 0 2rem;
 	display: flex;
+    flex-direction: row;
     margin-bottom: 9rem;
+    @media ${screens.laptopS} {
+		flex-direction: column;
+	}
+    @media ${screens.laptopS} {
+		padding: 8rem 1rem 0 1rem;
+	}
 `;
 const LeftContent = styled.div`
 	width: 50%;
 	padding-top: 2rem;
+    @media ${screens.laptopS} {
+		width: 100%;
+        margin-bottom: 3rem;
+	}
 `;
 
 const HeaderWrapper = styled.div`
 	display: flex;
-	/* align-items: flex-end; */
     margin-bottom: 1rem;
 `;
 
@@ -42,9 +53,17 @@ const HeaderDesc = styled.p`
     font-weight: 700;
     width: 80%;
     line-height: 1.2;
-    margin-bottom: 4rem;
+    margin-bottom: 3rem;
+    @media ${screens.tabletS} {
+        font-size: 2rem;
+    }
 `;
-const LeftButtons = styled.div``;
+const LeftButtons = styled.div`
+    display: flex;
+    @media ${screens.tabletS} {
+        flex-direction: column;
+    }
+`;
 const Button1 = styled(Link)`
     text-decoration: none;
     color: #fff;
@@ -54,6 +73,12 @@ const Button1 = styled(Link)`
     font-weight: 700;
     padding: 1rem 1.5rem;
     margin-right: 1rem;
+    width: 150px;
+    display: flex;
+    justify-content: center;
+    @media ${screens.tabletS}{
+        margin-bottom: 1rem;
+    }
 `;
 const Button2 = styled(Link)`
     text-decoration: none;
@@ -63,12 +88,18 @@ const Button2 = styled(Link)`
     font-size: 90%;
     font-weight: 700;
     padding: 1rem 1.5rem;
+    width: 150px;
+    display: flex;
+    justify-content: center;
 `;
 const RightContent = styled.div`
 	width: 50%;
 	display: flex;
 	overflow: hidden;
 	justify-content: center;
+    @media ${screens.laptopS} {
+		width: 100%;
+	}
 `;
 const RightImage = styled.div`
 	width: 450px;
@@ -81,21 +112,39 @@ const RightImage = styled.div`
 const AudioContent = styled.div`
     background-color: rgba(104, 99, 253, 0.2);
     padding: 2rem 3rem;
+    @media ${screens.laptopS} {
+		padding: 2rem;
+	}
+    @media ${screens.mobileL} {
+		padding: 2rem 1rem;
+	}
 `
 const AudioContainer = styled.div`
     display: flex;
+    flex-direction: row;
     align-items: center;
-    /* width: 100%; */
+    @media ${screens.laptopS} {
+		flex-direction: column;
+        align-items: flex-start;
+	}
 `
 const Artist = styled.div`
     display: flex;
     align-items: center;
     margin-right: 2rem;
     width: 50%;
+    @media ${screens.laptopS} {
+		margin-bottom: 2rem;
+        width: 100%;
+	}
 `
 const ArtistImage = styled.img`
     border-radius: 50%;
     margin-right: 1.5rem;
+    @media ${screens.tablet} {
+		width: 50px;
+        height: auto;
+	}
 `
 const ArtistDesc = styled.div`
     
@@ -111,9 +160,16 @@ const AudioPlayer = styled.div`
     align-items: center;
     justify-content: space-between;
     width: 100%;
+    @media ${screens.tabletS} {
+		flex-direction: column;
+        align-items: flex-start;
+	}
 `
 const AudioLinks = styled.div`
     display: flex;
+    @media ${screens.tabletS} {
+		margin-top: 2rem;
+	}
 `
 const AudioLink = styled(Link)`
     text-decoration: none;
