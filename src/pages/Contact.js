879638eac560d421fe21ceb5714b64532f90ca65
algofactor <johnsolutions4@gmail.com>
2022-03-31 23:49:30 +0100

    Fixed deployment bug

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ContactDetails from '../components/Contact/ContactDetails'
-import ContactMessage from '../components/Contact/ContactMessage'
 import HeroSection from '../components/Contact/HeroSection'
 
 const Contact = ({nav, showNav, closeNav}) => {
